b087ebee133213005cfa6c0a82d1bff6234f8c45
Jordan Smyth <jsmyth1423@gmail.com>
2022-02-18 16:54:47 +0000

    added comments for current js logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,25 @@
+// DOM connection to the grid
 const grid = document.querySelector('.grid');
 
-
-
+// Setting the size of the grid 
+//! will likely have to be adjusted? need to figure out div size scaling in css to keep even
 const width = 5;
 const gridCellCount = width * width;
 const cells = [];
 
+// Setting the initial ship location
 let shipLocation = 22
 
+// Functions to both add and remove the ship, using DOM css class editing
+function addShip() {
+  cells[shipLocation].classList.add('ship')
+}
+
+function removeShip() {
+  cells[shipLocation].classList.remove('ship')
+}
+
+// Function to duplicate divs to the size pre-determined above + give them ids so can be manipulated & calling the function to make the ship appear
 function createGrid() {
   for (let i = 0; i < gridCellCount; i++) {
     const cell = document.createElement('div');
@@ -18,23 +30,16 @@ function createGrid() {
   addShip()
 }
 
+
 createGrid()
 
-function addShip() {
-  cells[shipLocation].classList.add('ship')
-}
-
-function removeShip() {
-  cells[shipLocation].classList.remove('ship')
-}
-
 function playerControl(event) {
-  if (event.keyCode === 37) {
+  if (event.keyCode === 37) {  // 37 for left arrow key, if pressed do this
     removeShip()
     shipLocation--
     addShip()
     console.log('ship moved left')
-  } else if (event.keyCode === 39) {
+  } else if (event.keyCode === 39) { // 39 for right arrow key
     removeShip()
     shipLocation++
     addShip()
@@ -44,4 +49,5 @@ function playerControl(event) {
   }
 }
 
-document.addEventListener('keyup', playerControl)
+// making the DOM listen for a keyup event in which case it calls the playerControl function
+document.addEventListener('keyup', playerControl) 
